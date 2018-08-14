d4f9a63f339c0b683a09d3419f108cecc8dfaa94
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-14 08:41:25 -0700

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public string Nombre {get; set;}
+        public string Direccion { get; set; }
+        public string TelefonoCasa { get; set; }
+        public string CorreoElectronico { get; set; }
+
+        private string _telefonoCelular;
+        public string TelefonoCelular {
+            get
+            {
+                return _telefonoCelular;
+                    
+            }
+            set
+            {
+                if (value.Length == 10)
+                {
+                    _telefonoCelular = value;
+                } else if (value.Length == 7)
+                {
+                    _telefonoCelular = "644" + value;
+                } else
+                {
+                    Console.WriteLine("numero incorrecto");
+                }
+            }
+        }
+
+      
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1uno = new Class1();
+            class1uno.Nombre = "eduardo castro ";
+            class1uno.Direccion = "por ahi ";
+            class1uno.CorreoElectronico = "eduardoclx@gmail.com ";
+            class1uno.TelefonoCasa = "6622045320 ";
+            class1uno.TelefonoCelular = "4443493849 ";
+            Console.WriteLine("Nombre " + class1uno.Nombre);
+            Console.WriteLine("Direccion " + class1uno.Direccion);
+            Console.WriteLine("Celular " + class1uno.TelefonoCelular);
+            Console.WriteLine("Casa " + class1uno.TelefonoCasa);
+            Console.WriteLine("Correo " + class1uno.CorreoElectronico);
+            Console.ReadLine();
+
+
+        }
+    }
+}
